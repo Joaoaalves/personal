8cdcaffce33fd07a6ad2494cbbb96c191d462ecf
Joaoaalves <joaoaa21@hotmail.com>
2024-04-12 17:42:33 -0300

    Adicionado currículo ao site

--- pages/cv/index.js
+++ pages/cv/index.js
@@ -2,12 +2,16 @@ import Layout from "@/layouts/main";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 
+const pdfURL = '/curriculo.pdf';
 export default function CV() {
   const { t } = useTranslation();
   return (
     <Layout>
       <h1 className="text-3xl text-center">{t("cv.title")}</h1>
-      <p className="mt-8">{t("cv.description")}</p>
+      <div className="flex justify-center mt-4 lg:mt-12">
+        <iframe src={`${pdfURL}#toolbar=0`} width="800" height="1000" className="hidden lg:block"></iframe>
+        <iframe src={`${pdfURL}#toolbar=0`} width="300" height="600" className="flex lg:hidden bg-red-500"></iframe>
+      </div>
     </Layout>
   );
 }
